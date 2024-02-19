da929a7d63ed3103d1a4a8ccd20b9ee55cf02dde
shane <shaneshaji10@gmail.com>
2024-02-19 23:54:05 +0530

    fix: resolve bug in downloading 20 files

--- data_operations.py
+++ data_operations.py
@@ -1,15 +1,13 @@
 from zipfile import ZipFile
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 import os
 import constants as c
 
 
-def get_today_file():
-    # Get today's date
-    today = datetime.today()
-    url = f"{c.FILE_URL}{today.strftime('%Y')}/{today.strftime('%b').upper()}"
-    date_str = today.strftime("%d%b%Y").upper()
+def get_file(date):
+    url = f"{c.FILE_URL}{date.strftime('%Y')}/{date.strftime('%b').upper()}"
+    date_str = date.strftime("%d%b%Y").upper()
     filename = "cm{}bhav.csv.zip".format(date_str)
 
     # GET Request to URL for downloading the file
@@ -24,7 +22,40 @@ def get_today_file():
     with ZipFile(filename, 'r') as zip_ref:
         zip_ref.extractall()
     os.remove(filename)
+    today_file_path = os.path.abspath(filename[0:-4])
+    return today_file_path
+
+
+def get_dates():
+    # Get today's date
+    today = datetime.today()
+    date_range = []
+
+    files_list = []
+
+    current_date = today
+    index = 0
+    while index < 20:
+        os.system('cls')
+        print(f"{index+1}/20")
+
+        # Check if the current date is a weekday (Monday to Friday)
+        if current_date.weekday() < 5:
+            date_range.append(current_date)
+
+            #Download file for that particular date if it isn't a holiday
+            try:
+                file_path = get_file(current_date)
+                files_list.append(file_path)
+                index += 1
+            except Exception as e:
+                
+                print(f"Error getting file for date {current_date}: {e}")
+
+        # Move to the previous day
+        current_date -= timedelta(days=1)
+    return files_list
 
 
 if __name__ == "__main__":
-    get_today_file()
+    print(get_dates())
