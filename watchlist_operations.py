97b23a90445f54dd622d4d5f34ee867c94f3c400
shane <shaneshaji10@gmail.com>
2024-02-19 21:33:17 +0530

    chore: switched webdriver from Edge to Chrome Switched the Selenium webdriver to use Chrome instead of Edge for wider usage.

--- watchlist_operations.py
+++ watchlist_operations.py
@@ -1,5 +1,7 @@
-from seleniumwire import webdriver  # Import from seleniumwire
+from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import json
 import constants as c
 
 def DMA200(company):
@@ -7,15 +9,18 @@ def DMA200(company):
 
     # Create a new instance of the driver
     print("\nCREATING BROWSER INSTANCE")
-    options = webdriver.EdgeOptions()
+
+    options = webdriver.ChromeOptions()
     options.add_argument("--log-level=3")
-    driver = webdriver.Edge(options=options)
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
 
     # Go to the screener page
     print(f"\nSCRAPING DATA FOR {company}")
     driver.get(f'{c.BASE_URL}/company/{company}/')
 
     # Access requests via the `requests` attribute
+    
     for request in driver.requests:
         if request.response and request.url[-3::] == '365':
             company_code = request.url[36:40]
@@ -23,8 +28,11 @@ def DMA200(company):
             break
     driver.get(
         f'{c.BASE_URL}/api/company/{company_code}/{c.API_QUERY}')
-    DMA200 = float(driver.find_element(
-        By.CSS_SELECTOR, "body > div.cm-editor.ͼ1.ͼ3.ͼ4.ͼ6.ͼ5 > div.cm-scroller > div.cm-content.cm-lineWrapping > div:nth-child(9) > span").text[1:-1])
+    driver.get_screenshot_as_file("screenshot.png")
+    content = (driver.find_element(By.CSS_SELECTOR, "body pre").text)
+    data = json.loads(content)
+    
+    DMA200 = data['datasets'][0]['values'][0][1]
     print(DMA200)
     driver.quit()
     return DMA200
