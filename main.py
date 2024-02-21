c71c81295718a9927adc9ef05870a16acf4c3c2d
shane <shaneshaji10@gmail.com>
2024-02-21 16:21:26 +0530

    feat: integrate operations in main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import constants as c
-import data_operations
-import watchlist_operations
+from data_operations import *
+from watchlist_operations import *
+
+if __name__ == "__main__":
+    downloaded_files = download_files()
+    files_list = downloaded_files[0]
+    latest_bhavcopy = downloaded_files[1]
+    query(latest_bhavcopy, files_list)
+    print("Final shortlisted companies are listed in Results.xlsx")
+
 
