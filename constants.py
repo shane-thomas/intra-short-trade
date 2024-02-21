25daa172d119cace0d9d6230f55ccf187d803885
shane <shaneshaji10@gmail.com>
2024-02-21 18:53:26 +0530

    chore: removed unnecessary columns

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 BASE_URL = "https://www.screener.in"
 API_QUERY = "chart/?q=DMA200&days=1"
 FILE_URL = "https://archives.nseindia.com/content/historical/EQUITIES/"
-DEL_COLUMNNS = ['ISIN', 'LOW', 'HIGH', 'OPEN', 'PREVCLOSE', 'TIMESTAMP', 'TOTALTRADES', 'TOTTRDVAL']
+DEL_COLUMNNS = ['ISIN', 'LOW', 'HIGH', 'OPEN', 'PREVCLOSE', 'TIMESTAMP', 'TOTALTRADES', 'TOTTRDVAL', 'SERIES', 'LAST', 'Unnamed: 13']
